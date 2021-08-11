0381f88a0f6abd78883a72779b0880e507ba9ae5
Laryea Quaye <laryea.quaye@instrument.com>
2021-08-11 11:46:50 -0700

    pulling html from dev.to

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
+"use strict";
 const cheerio = require('cheerio');
-const $ = cheerio.load('<h2 class="title">Hello world</h2>');
+const axios = require('axios');
 
-$('h2.title').text('Hello there!');
-$('h2').addClass('welcome');
+(async () => {
+    try {
+        const url = 'http://dev.to'
+        console.debug(`fetching ${url}`)
+        const response = await axios.get(url);
+        const html = response.data
+        
+        const $ = cheerio.load(html);
+        
+        $('h2.title').text('Hello there!');
+        $('h2').addClass('welcome');
+        
+        console.log($.html());
+    } catch (error) {
+        console.log(error)
+    }
+})()
 
-console.log($.html());
